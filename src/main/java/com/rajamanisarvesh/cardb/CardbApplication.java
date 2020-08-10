8372b62e12e382345b5380acc0dd4f2483ecae0b
Rajamani Sarvesh <rajamanisarvesh23@gmail.com>
2020-08-10 13:12:26 -0700

    Initial commit

---
+++ src/main/java/com/rajamanisarvesh/cardb/CardbApplication.java
@@ -0,0 +1,13 @@
+package com.rajamanisarvesh.cardb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rajamanisarvesh/cardb/CardbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajamanisarvesh.cardb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
